c7db532f41a71a248e4abdf51bb9baacfbf4b458
Baltasar Jiménez Rincón <baltax.75@gmail.com>
2020-03-31 02:58:41 +0200

    Login hecho

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -8,11 +12,21 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  user = new FormGroup({
+    username: new FormControl('asd', [Validators.required]),
+    password: new FormControl('asdasdasd', [Validators.required])
+  });
+
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    public userService: UserService
   ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(user: FormGroup) {
+    this.userService.getUser(user.value);
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { User } from '../interfaces/User';
 
+import Swal from 'sweetalert2'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +14,36 @@ export class UserService {
 
   constructor(private db: AngularFirestore) { }
 
+  getUsers() {
+    return this.db.collection("user").get();
+  }
+
+  getUser(data) {
+    return this.db.collection("user", ref => ref.where('username', '==', data.username).where('password', '==', data.password)).get().subscribe((result) => {
+      if (result.empty == false) {
+        this.currentUser = {
+          id: result.docs[0].id,
+          username: result.docs[0].data().username,
+          password: result.docs[0].data().password,
+          routes: result.docs[0].data().routes,
+        }
+
+        Swal.fire(
+          'Succesfully Logged!',
+          'You have been succesfully logged!',
+          'success'
+        )
+      } else {
+        Swal.fire(
+          'Error!',
+          'You have been succesfully registered!',
+          'error'
+        )
+      }
+    })
+  }
+
   insertUser(data) {
     return this.db.collection("user").add(data);
-  }
+  };
 }
